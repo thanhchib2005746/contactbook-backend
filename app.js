8d96de62fce930e65dcd5aedb782dfcbc5777c86
thanhchib2005746 <chib2005746@student.ctu.edu.vn>
2023-09-18 23:11:39 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
-
+const contactsRouter = require("./app/routes/contact.route");
+// middleware
 app.use(cors());
 app.use(express.json());
-
+// routes
+app.use("/api/contacts", contactsRouter);
+// run server
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application."});
 });
